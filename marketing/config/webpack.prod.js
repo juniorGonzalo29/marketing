c557b0339edd6df351a4adcafc17a04bbfceaa2d
korn.44.junior29@gmail.com <korn.44.junior29@gmail.com>
2023-02-25 00:54:12 -0500

    repo23

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/marketing/public/',
+    publicPath: '/marketing/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
